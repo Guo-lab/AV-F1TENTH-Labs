87eef7674af8be33420531f2b9728a5c8492ba2a
Siqi Guo, Edward <siqiguo@andrew.cmu.edu>
2024-07-15 01:11:01 -0400

    gap follower done, levine_blocked passed, levine_obs failed... how to pass this test...

--- lab4_ws/src/gap_follow/src/reactive_node.cpp
+++ lab4_ws/src/gap_follow/src/reactive_node.cpp
@@ -11,9 +11,12 @@ using std::placeholders::_1;
 
 #include <cmath>
 #define HALF_PI (M_PI / 2.0)
+
+/** Disparity Extender @ref: PJmin2 */
 #define FULL_RANGE 1080
-#define RANGE_MIN 200
-#define RANGE_MAX 900
+#define RANGE_MIN 300
+#define RANGE_MAX 800
+#define MAX_DIST 3.5
 
 /**
  * ========================== Helper Function ========================
@@ -36,6 +39,26 @@ void smooth_ranges_with_mean(std::vector<float>& ranges, int window_size) {
     }
     // std::cout << "Smoothed Ranges: " << smoothed_ranges.size() << std::endl;
     ranges = smoothed_ranges;
+}
+
+/** ===================== Corner Case Helper Function ==================
+ * @ref: dhanvi97
+ * @brief: These two functions are used to help robot pass the corner
+ */
+int corner_checker(const std::vector<float>& preprocessed_ranges, int left_side_idx, int right_side_idx) {
+    for (int i = 0; i < left_side_idx; i++)
+        if (preprocessed_ranges.at(i) < 0.1) return -1;
+    for (int i = right_side_idx; i < (int)preprocessed_ranges.size(); i++)
+        if (preprocessed_ranges.at(i) < 0.1) return 1;
+    return 0;
+}
+
+bool steering_override_at_corner(int corner_coll, double best_steering_angle) {
+    if (corner_coll == 0) {
+        return false;
+    } else {
+        return (corner_coll * best_steering_angle) > 0;
+    }
 }
 
 class TimeNode : public rclcpp::Node {
@@ -66,7 +89,7 @@ class ReactiveFollowGap : public rclcpp::Node {
             lidarscan_topic, 10, std::bind(&ReactiveFollowGap::lidar_callback, this, _1));
 
         ackermann_drive_pub = this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>(drive_topic, 10);
-    
+
         timer_node_->GetCurrentTime();
     }
 
@@ -81,31 +104,41 @@ class ReactiveFollowGap : public rclcpp::Node {
     ackermann_msgs::msg::AckermannDriveStamped drive_msg = ackermann_msgs::msg::AckermannDriveStamped();
     std::shared_ptr<TimeNode> timer_node_ = std::make_shared<TimeNode>();
 
+    double fov_start_idx = 0.0;
+    double fov_end_idx = 0.0;
+
     void preprocess_lidar(const std::vector<float>& ranges) {
         // Preprocess the LiDAR scan array. Expert implementation includes:
+
         // 1.Setting each value to the mean over some window
         const int window_size = 5;
+
         // 2.Rejecting high values (eg. > 3m)
 
         preprocessed_ranges.clear();
         for (size_t i = 0; i < ranges.size(); i++) {
-            // if (i < RANGE_MIN || i > RANGE_MAX) {
-            //     preprocessed_ranges.push_back(0.0);
-            //     continue;
-            // }
-            if (std::isnan(ranges[i]) || !std::isfinite(ranges[i]) || ranges[i] > 3.0) {
-                preprocessed_ranges.push_back(3.0);
+            if (i < RANGE_MIN || i > RANGE_MAX) {
+                preprocessed_ranges.push_back(0.0);
+                continue;
+            }
+            if (std::isnan(ranges[i]) || !std::isfinite(ranges[i]) || ranges[i] > MAX_DIST) {
+                preprocessed_ranges.push_back(MAX_DIST);
                 continue;
             }
             preprocessed_ranges.push_back(ranges[i]);
         }
-        // std::cout << "Original Ranges: " << preprocessed_ranges.size() << std::endl;
         smooth_ranges_with_mean(preprocessed_ranges, window_size);
-        // std::cout << "Preprocessed Ranges: " << preprocessed_ranges.size() << std::endl;
-        // for (auto each_range: preprocessed_ranges) {
-        //     std::cout << each_range << " ";
-        // }
-        // std::cout << std::endl;
+
+        // Disparity extender (might not be that helpful here)
+        const float disparity_threshold = 2;
+        for (size_t i = 1; i < preprocessed_ranges.size(); i++) {
+            double disparity = std::abs(preprocessed_ranges[i] - preprocessed_ranges[i - 1]);
+            if (disparity > disparity_threshold) {
+                for (size_t j = i; j < preprocessed_ranges.size() && disparity > disparity_threshold; j++) {
+                    preprocessed_ranges[j] = preprocessed_ranges[j - 1];
+                }
+            }
+        }
         return;
     }
 
@@ -113,11 +146,12 @@ class ReactiveFollowGap : public rclcpp::Node {
         // Return the start index & end index of the max gap in free_space_ranges
         int max_start = 0, max_end = 0, current_start = 0;
         bool in_gap = false;
-        for (size_t i = 0; i < ranges.size(); i++) {
-            if (ranges[i] > 0.3 && !in_gap) { // not 0.0, for moving forward
+
+        for (size_t i = RANGE_MIN; i <= RANGE_MAX; i++) {
+            if (ranges[i] > 1.5 && !in_gap) {  // 1.5 also an important parameter
                 current_start = i;
                 in_gap = true;
-            } else if (ranges[i] <= 0.3 && in_gap) {
+            } else if (ranges[i] <= 1.5 && in_gap) {
                 if ((int)i - current_start > max_end - max_start) {
                     max_start = current_start;
                     max_end = i;
@@ -125,9 +159,9 @@ class ReactiveFollowGap : public rclcpp::Node {
                 in_gap = false;
             }
         }
-        if (in_gap && (int)ranges.size() - current_start > max_end - max_start) {
+        if (in_gap && RANGE_MAX - current_start > max_end - max_start) {
             max_start = current_start;
-            max_end = ranges.size();
+            max_end = RANGE_MAX;
         }
         return {max_start, max_end};
     }
@@ -136,41 +170,50 @@ class ReactiveFollowGap : public rclcpp::Node {
         // Start_i & end_i are start and end indicies of max-gap range, respectively
         // Return index of best point in ranges
         // Naive: Choose the furthest point within ranges and go there
+
         int best_point = start_idx;
         for (int i = start_idx; i < end_idx; i++) {
             if (ranges[i] > ranges[best_point]) {
                 best_point = i;
             }
         }
-        return best_point;
+        // return best_point;
+
+        return (start_idx + end_idx) / 2;
     }
 
     auto GetSpeed(double steering_angle) -> double {
         double abs_angle = std::abs(steering_angle);
         if (abs_angle >= 0.0 * (M_PI / 180.0) && abs_angle <= 10.0 * (M_PI / 180.0)) {
-            return 1.5;
+            return 3;
         }
         if (abs_angle > 10.0 * (M_PI / 180.0) && abs_angle <= 20.0 * (M_PI / 180.0)) {
-            return 1.0;
+            return 2;
         }
-        return 0.5;
+        return 1;
     }
 
     void lidar_callback(const sensor_msgs::msg::LaserScan::ConstSharedPtr scan_msg) {
         // Process each LiDAR scan as per the Follow Gap algorithm & publish an AckermannDriveStamped Message
         preprocess_lidar(scan_msg->ranges);
 
+        fov_start_idx = ((-M_PI / 2) - scan_msg->angle_min) / scan_msg->angle_increment;
+        fov_end_idx = ((M_PI / 2) - scan_msg->angle_min) / scan_msg->angle_increment;
+
         /// TODO:
         // Find closest point to LiDAR
-        auto min_it = std::min_element(preprocessed_ranges.begin(), preprocessed_ranges.end());
+        auto min_it =
+            std::min_element(preprocessed_ranges.begin() + RANGE_MIN, preprocessed_ranges.begin() + RANGE_MAX);
         int closest_point_idx = std::distance(preprocessed_ranges.begin(), min_it);
-        
+
         // Eliminate all points inside 'bubble' (set them to zero)
-        const int bubble_radius = 5;
-        int left_idx = std::max(0, closest_point_idx - bubble_radius);
-        int right_idx = std::min((int)preprocessed_ranges.size(), closest_point_idx + bubble_radius);
-        for (int i = left_idx; i < right_idx; i++) {
-            preprocessed_ranges[i] = 0.0;
+        if (scan_msg->ranges.at(closest_point_idx) < MAX_DIST) {
+            const int bubble_radius = 8;
+            int left_idx = std::max(RANGE_MIN, closest_point_idx - bubble_radius);
+            int right_idx = std::min(RANGE_MAX, closest_point_idx + bubble_radius);
+            for (int i = left_idx; i <= right_idx; i++) {
+                preprocessed_ranges[i] = 0.0;
+            }
         }
 
         // Find max length gap
@@ -178,18 +221,33 @@ class ReactiveFollowGap : public rclcpp::Node {
 
         // Find the best point in the gap
         int best_point = find_best_point(scan_msg->ranges, start_i, end_i);
+
+        // Logging Data
         if (timer_node_->IfLogging()) {
             timer_node_->GetCurrentTime();
             RCLCPP_INFO(this->get_logger(), "Range Size: %ld", scan_msg->ranges.size());
             RCLCPP_INFO(this->get_logger(), "Gap: [%d, %d]", start_i, end_i);
             RCLCPP_INFO(this->get_logger(), "Best point: %d", best_point);
-            RCLCPP_INFO(this->get_logger(), "angle: %f", scan_msg->angle_min + best_point * scan_msg->angle_increment);
+            RCLCPP_INFO(this->get_logger(), "angle: %f degrees",
+                        (scan_msg->angle_min + best_point * scan_msg->angle_increment) * (180.0 / M_PI));
+            RCLCPP_INFO(this->get_logger(), "Angle Increment: %f", scan_msg->angle_increment);
         }
-        
+
         // Publish Drive message
         double angle = scan_msg->angle_min + best_point * scan_msg->angle_increment;
         drive_msg.drive.steering_angle = angle;
+        if (scan_msg->ranges.at(best_point) < 1) {
+            angle = 0.0;
+        }
+
         drive_msg.drive.speed = GetSpeed(angle);
+
+        int corner_collision = corner_checker(preprocessed_ranges, fov_start_idx, fov_end_idx);
+        if (steering_override_at_corner(corner_collision, angle)) {
+            drive_msg.drive.steering_angle = 0.0;
+            drive_msg.drive.speed = 1.0;
+        }
+
         ackermann_drive_pub->publish(drive_msg);
     }
 };
